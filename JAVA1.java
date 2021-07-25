ebfe657991cbecc0a5ed26aba024f1705647c352
ASHITA GOYAL <81897390+ashitagoyal-88009@users.noreply.github.com>
2021-07-25 19:28:39 +0530

    Create JAVA1.java

---
+++ JAVA1.java
@@ -0,0 +1,16 @@
+  
+import java.io.*;
+ class SumElements{
+public static void main(String args[])
+{
+	int num,sum=0;
+	System.out.println("Command line Arguments are 10,20,30");
+	for(int i=0;i<args.length;i++)
+	{
+	num=Integer.parseInt(args[i]);
+	sum=sum+num;
+	}
+	System.out.print("Sum of The Elements is:" +sum);
+
+}
+}
